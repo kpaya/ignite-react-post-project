54ebac69bf191a78d8e24e62fa52be93c1cd76b4
Kpaya <matteus.santos03@gmail.com>
2023-08-12 11:24:39 -0300

    feat: add sidebar

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,9 +1,28 @@
+"use client"
+
 import styles from './Sidebar.module.css';
+import { PencilLine } from 'phosphor-react';
 
 export function Sidebar() {
     return (
-        <aside className='sidebar'>
+        <>
+            <aside className={styles.sidebar}>
+                <img className={styles.cover} src="https://images.unsplash.com/photo-1451187580459-43490279c0fa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=40" />
 
-        </aside>
+
+                <div className={styles.profile}>
+                    <img className={styles.avatar} src="https://github.com/kpaya.png" />
+                    <strong>Kpaya</strong>
+                    <span>Software Engineering</span>
+                </div>
+
+                <footer>
+                    <a href="#">
+                        <PencilLine size={20} />
+                        Editar Perfil
+                    </a>
+                </footer>
+            </aside>
+        </>
     )
 }
