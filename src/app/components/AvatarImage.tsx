5011137b8be61f7eae3e18a1559347e811aad606
Kpaya <matteus.santos03@gmail.com>
2023-08-13 15:30:20 -0300

    feat: add new attrs to avatarImage component

--- src/app/components/AvatarImage.tsx
+++ src/app/components/AvatarImage.tsx
@@ -1,9 +1,10 @@
-import styles from "./AvatarImage.module.css";
+import styles from "./AvatarImage.module.css"
+import Image from 'next/image'
 
-export function AvatarImage({ src }: { src: string }) {
+export function AvatarImage({ src, widthSize = "3rem", heightSize = "3rem" }: { src: string, widthSize: string, heightSize: string }) {
     return (
         <>
-            <img className={styles.avatar} src={src} />
+            <img className={styles.avatar} style={{ width: widthSize, height: heightSize }} src={src} />
         </>
     )
 }
--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -11,7 +11,7 @@ export function Sidebar() {
                 <img className={styles.cover} src="https://images.unsplash.com/photo-1451187580459-43490279c0fa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=40" />
 
                 <div className={styles.profile}>
-                    <AvatarImage src='https://github.com/kpaya.png' />
+                    <AvatarImage src='https://github.com/kpaya.png' widthSize='calc(3rem + 12px)' heightSize='calc(3rem + 12px)' />
                     <strong>Kpaya</strong>
                     <span>Software Engineering</span>
                 </div>
